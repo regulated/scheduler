748c54a3df3efd9749e5da51b6ab6c2f9d685630
Elliot <eschem58@gmail.com>
2026-09-11 09:36:40 -0500

    working state check

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect, useMemo } from "react";
 
-import { Field, FieldContent, FieldDescription, FieldLabel, FieldTitle } from "@/components/ui/field"
+import { Field, FieldSet, FieldContent, FieldDescription, FieldLabel, FieldTitle } from "@/components/ui/field"
 import { Label } from "@/components/ui/label"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 
@@ -26,12 +26,21 @@ export default function Home() {
     const seconds = time.getSeconds().toString().padStart(2, "0");
     return `${hours}:${minutes}:${seconds}`; 
   }, [time, mounted]); 
+
+  const minutes = useMemo<number>(() => {
+    if (!mounted) return "";
+    return time.getMinutes();
+  }, [time, mounted]);
+
+  const [selectedValue, setSelectedValue] = useState("first");
   
   return (
     <div className="flex m-12 justify-center items-center flex-col">
 
-          <RadioGroup defaultValue="first" className="max-w-m flex flex-col-6 justify-between">
-            <FieldLabel htmlFor="r1">
+      <RadioGroup defaultValue="first" value={selectedValue} onValueChange={setSelectedValue} className="max-w-m flex flex-col-6 justify-between">
+        
+          <FieldSet className={(minutes < 10 && selectedValue != "first") ? "bg-orange-900 w-full" : "w-full"}>
+            <FieldLabel htmlFor="r1" >
               <Field orientation="vertical">
                 <FieldContent>
                   <FieldTitle>Problemas</FieldTitle>
@@ -39,7 +48,9 @@ export default function Home() {
                   <RadioGroupItem value="first" id="r1" />
               </Field>
             </FieldLabel>
-            
+          </FieldSet>
+              
+          <FieldSet className={(minutes >= 10 && minutes < 20 && selectedValue != "second") ? "bg-orange-900 w-full" : "w-full"}>
             <FieldLabel htmlFor="r2">
               <Field orientation="vertical">
                 <FieldContent>
@@ -48,7 +59,9 @@ export default function Home() {
                   <RadioGroupItem value="second" id="r2" />
               </Field>
             </FieldLabel>
+          </FieldSet>
 
+          <FieldSet className={(minutes >= 20 && minutes < 30 && selectedValue != "third") ? "bg-orange-900 w-full" : "w-full"}>
             <FieldLabel htmlFor="r3">
               <Field orientation="vertical">
                 <FieldContent>
@@ -57,7 +70,9 @@ export default function Home() {
                   <RadioGroupItem value="third" id="r3" />
               </Field>
             </FieldLabel>
+          </FieldSet>
 
+          <FieldSet className={(minutes >= 30 && minutes < 40 && selectedValue != "fourth") ? "bg-orange-900 w-full" : "w-full"}>
             <FieldLabel htmlFor="r4">
               <Field orientation="vertical">
                 <FieldContent>
@@ -66,7 +81,9 @@ export default function Home() {
                   <RadioGroupItem value="fourth" id="r4" />
               </Field>
             </FieldLabel>
+          </FieldSet>
             
+          <FieldSet className={(minutes >= 40 && minutes < 50 && selectedValue != "fifth") ? "bg-orange-900 w-full" : " w-full"}>
             <FieldLabel htmlFor="r5">
               <Field orientation="vertical">
                 <FieldContent>
@@ -75,7 +92,9 @@ export default function Home() {
                   <RadioGroupItem value="fifth" id="r5" />
               </Field>
             </FieldLabel>
+          </FieldSet>
 
+          <FieldSet className={(minutes >= 50 && selectedValue != "sixth") ? "bg-orange-900 w-full" : " w-full"}>
             <FieldLabel htmlFor="r6">
               <Field orientation="vertical">
                 <FieldContent>
@@ -84,10 +103,11 @@ export default function Home() {
                   <RadioGroupItem value="sixth" id="r6" />
               </Field>
             </FieldLabel>
+          </FieldSet>
         
-          </RadioGroup>
+        </RadioGroup>
 
-          <div>{formattedTime}</div>
+        <div className="p-4">{formattedTime}</div>
 
       
     </div>
